1f8be927fe3dccbd9cc6c4929052362fba64b909
PichaevIvan <ivan.pichaev@mail.ru>
2024-11-19 12:43:25 +0300

    add task2

--- ConsoleApplication8.cpp
+++ ConsoleApplication8.cpp
@@ -1,165 +1,176 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <ctime>
+#include <time.h>
 #include <cstdlib>
 #include <windows.h>
 
-#define MAX_VERTICES 100
+using namespace std;
 
 struct node {
-	int vertex;
+	int inf;
 	struct node* next;
 };
 
-struct Graph {
-	int numVertices;
-	struct node** adjLists;
-};
+struct node* head = NULL, * last = NULL;
 
-struct node* createNode(int v) {
-	struct node* newNode = (node*)malloc(sizeof(struct node));
-	newNode->vertex = v;
-	newNode->next = NULL;
-	return newNode;
-}
+void BFSqueue(vector < vector < int >> G, int start) {
+	int n = G.size();
+	vector<bool> visited(n, false);
+	queue<int> Q;
 
-struct Graph* createAGraph(int vertices) {
-	struct Graph* graph = (Graph*)malloc(sizeof(struct Graph));
-	graph->numVertices = vertices;
+	Q.push(start);
+	visited[start] = true;
 
-	graph->adjLists = (node**)malloc(vertices * sizeof(struct node*));
+	while (!Q.empty()) {
+		int v = Q.front();
+		Q.pop();
+		cout << v + 1 << " ";
 
-	for (int i = 0; i < vertices; i++) {
-		graph->adjLists[i] = NULL;
-	}
-
-	return graph;
-}
-
-void addEdge(struct Graph* graph, int s, int d) {
-	struct node* temp = graph->adjLists[s];
-	while (temp) {
-		if (temp->vertex == d) {
-			return;
-		}
-		temp = temp->next;
-	}
-
-	struct node* newNode = createNode(d);
-	newNode->next = graph->adjLists[s];
-	graph->adjLists[s] = newNode;
-
-	temp = graph->adjLists[d];
-	while (temp) {
-		if (temp->vertex == s) {
-			return;
-		}
-		temp = temp->next;
-	}
-
-	newNode = createNode(s);
-	newNode->next = graph->adjLists[d];
-	graph->adjLists[d] = newNode;
-}
-
-
-struct Graph* edges(int** G, int size) {
-	struct Graph* graph = createAGraph(size);
-
-	for (int i = 0; i < size; i++) {
-		for (int j = i; j < size; j++) {
-			if (G[i][j] == 1) {
-				addEdge(graph, i, j);
+		for (int i = 0; i < n; ++i) {
+			if (G[v][i] == 1 && !visited[i]) {
+				Q.push(i);
+				visited[i] = true;
 			}
 		}
 	}
-	return graph;
 }
 
-void printGraph(struct Graph* graph) {
-	for (int v = 0; v < graph->numVertices; v++) {
-		struct node* temp = graph->adjLists[v];
-		printf("\nVertex %d: ", v + 1);
-		while (temp) {
-			printf("%d -> ", temp->vertex + 1);
-			temp = temp->next;
+void enqueue(int value) {
+	struct node* p = (struct node*)malloc(sizeof(struct node));
+	if (p == NULL) {
+		printf("Ошибка при распределении памяти\n");
+		exit(1);
+	}
+	p->inf = value;
+	p->next = NULL;
+
+	if (last == NULL) {
+		head = p;
+		last = p;
+	}
+	else {
+		last->next = p;
+		last = p;
+	}
+}
+
+int isQueueEmpty(void) {
+	return head == NULL;
+}
+
+int dequeue(void) {
+	if (isQueueEmpty()) {
+		printf("Очередь пуста\n");
+		return -1;
+	}
+	struct node* temp = head;
+	int value = temp->inf;
+	head = head->next;
+	if (head == NULL) {
+		last = NULL;
+	}
+	free(temp);
+	return value;
+}
+
+void BFS(int** G, int n, int start) {
+	int* visited = (int*)malloc(n * sizeof(int));
+
+	for (int i = 0; i < n; i++) visited[i] = 0;
+
+	enqueue(start);
+	visited[start] = 1;
+
+	while (!isQueueEmpty()) {
+		int v = dequeue();
+		printf("%d ", v + 1);
+
+		for (int i = 0; i < n; i++) {
+			if (G[v][i] == 1 && !visited[i]) {
+				enqueue(i);
+				visited[i] = 1;
+			}
 		}
-		printf("\n");
 	}
-	printf("\n");
+	free(visited);
 }
 
-int** createG(int size) {
-	int** G = NULL;
-	G = (int**)malloc(size * sizeof(int*));
-	for (int i = 0; i < size; i++) {
-		G[i] = (int*)malloc(size * sizeof(int));
+int** generateG(int versh) {
+	int** G = (int**)malloc(versh * sizeof(int*));
+	for (int i = 0; i < versh; i++) {
+		G[i] = (int*)malloc(versh * sizeof(int));
+		for (int j = 0; j < versh; j++) {
+			G[i][j] = 0;
+		}
 	}
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			G[i][j] = rand() % 2;
-			if (i == j) G[i][j] = 0;
-			G[j][i] = G[i][j];
+
+	for (int i = 0; i < versh; i++) {
+		for (int j = i + 1; j < versh; j++) {
+			if (rand() % 2 == 1) {
+				G[i][j] = 1;
+				G[j][i] = 1;
+			}
 		}
 	}
 	return G;
 }
 
-void printG(int** G, int size) {
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
+void printG(int** G, int versh) {
+	printf("Матрица смежности: \n");
+	for (int i = 0; i < versh; i++) {
+		for (int j = 0; j < versh; j++) {
 			printf("%d ", G[i][j]);
 		}
 		printf("\n");
 	}
-	printf("\n");
-	return;
 }
 
-void BFS(struct Graph* graph, int start) {
-    bool visited[MAX_VERTICES] = { false };
-	int queue[MAX_VERTICES];
-    int front = 0, rear = 0;
-
-    visited[start] = true;
-    queue[rear++] = start;
-
-    printf("Результат обхода в ширину: : ", start + 1);
-
-    while (front < rear) {
-        int currentVertex = queue[front++];
-        printf("%d ", currentVertex + 1);
-
-        struct node* temp = graph->adjLists[currentVertex];
-        while (temp) {
-            int adjVertex = temp->vertex;
-            if (!visited[adjVertex]) {
-                visited[adjVertex] = true;
-                queue[rear++] = adjVertex;
-            }
-            temp = temp->next;
-        }
-    }
-    printf("\n");
-}
-
-int main(void) {
+int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	int sizeG1 = 0;
-	int** G1 = NULL;
+	srand(5435234623462343726);
 
+	int versh;
 	printf("Введите количество вершин графа: ");
-	scanf("%d", &sizeG1);
-	G1 = createG(sizeG1);
-	printf("1 graf\n");
-	printG(G1, sizeG1);
-	struct Graph* graph1 = edges(G1, sizeG1);
-	printGraph(graph1);
+	scanf("%d", &versh);
 
-	BFS(graph1, 0);
+	int** G = NULL;
+	G = generateG(versh);
+	printG(G, versh);
+
+	printf("\nОбход в ширину (Очередь на основе списка из лабы 3):\n");
+	clock_t start, end;
+
+	start = clock();
+	BFS(G, versh, 0);
+	end = clock();
+
+	float bfs = (double)(end - start) / CLOCKS_PER_SEC;
+
+	printf("\nВремя выполнения (Очередь на основе списка из лабы 3): %f секунд\n", bfs);
+
+
+	vector < vector < int >> G1(versh, vector<int>(versh, 0));
+	for (int i = 0; i < versh; i++) {
+		for (int j = 0; j < versh; j++) {
+			G1[i][j] = G[i][j];
+		}
+	}
+
+	cout<< endl << "Обход в ширину (Queue):" << endl;
+
+	start = clock();
+	BFSqueue(G1, 0);
+	end = clock();
+
+	float bfsQ = (double)(end - start) / CLOCKS_PER_SEC;
+
+	printf("\nВремя выполнения (Queue): %f секунд\n", bfsQ);
+	printf("\nВремя выполнения (Queue: %f) - (Очередь на основе списка из лабы 3: %f): %f секунд\n", bfsQ, bfs, bfsQ - bfs);
 }
